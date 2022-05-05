2a08aed133433d522dd0f692e38b0f1665043ed1
alumno <alumno@pc01-t1.inf>
2022-05-05 17:22:55 +0200

    bot

---
+++ src/probar/spammer.java
@@ -0,0 +1,41 @@
+
+package probar;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.util.Scanner;
+
+public class spammer {
+    public static void main(String[] args) throws AWTException, InterruptedException {
+        int x=1;
+        Robot robot = new Robot();
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Dime el texto para spamear jeje");
+        String texto = sc.nextLine();
+        
+        StringSelection st = new StringSelection(texto);
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.getContents(st,null);
+        
+        System.out.println("Wait 5 sec");
+        Thread.sleep(5000);
+        while(x <=10){
+            Thread.sleep(100);
+            robot.keyPress(KeyEvent.VK_CONTROL);
+            robot.keyPress(KeyEvent.VK_V);
+            
+            robot.keyRelease(KeyEvent.VK_CONTROL);
+            robot.keyRelease(KeyEvent.VK_V);
+            
+            robot.keyPress(KeyEvent.VK_ENTER);
+            robot.keyRelease(KeyEvent.VK_ENTER);
+            x++;
+            
+        }
+    }
+}
